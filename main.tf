417e72ad12c66d6cda526feaa56a1c8adb9118dc
Subhamay Bhattacharyya <142895397+bsubhamay@users.noreply.github.com>
2025-05-10 01:54:58 +0000

    changed something.

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
   }
 }
 
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_sse_configuration" {
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
